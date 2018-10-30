5fbce67ac40d7faec6b38286b12927f206609185
palonsovazquez <pav.vigo@gmail.com>
2018-10-30 10:58:52 +0100

    terminado boletin 6_2

---
+++ Boletin6_2/src/boletin6_2/Boletin6_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_2;
+
+/**
+ *
+ * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
+ */
+public class Boletin6_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Short numeroPrimero =  Peticiones.pedirNumeroShort("Introduzca el primer numero:");
+        Short numeroSegundo =  Peticiones.pedirNumeroShort("Introduzca el segundo numero:");
+        
+        if (numeroPrimero >= numeroSegundo){
+            System.out.println("La resta de los dos numeros es : "+ (numeroPrimero - numeroSegundo));
+        }
+        System.out.println("La suma de los dos numeros es : ");
+    }
+
+}
