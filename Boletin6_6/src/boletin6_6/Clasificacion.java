e8c8f60f1557277f2d8894422f440d1e69a4b780
palonsovazquez <pav.vigo@gmail.com>
2018-10-31 10:50:59 +0100

    prueba1

---
+++ Boletin6_6/src/boletin6_6/Clasificacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
+ */
+public class Clasificacion {
+    private static final String[] clasifcaciones = {"bajo","medio","alto","primera necesidad"};
+    
+    
+    
+}
